46ff8f488bec51ccb7de73a63aa691294ffc2635
md <mihkal@dunfjeld.no>
2016-12-16 17:02:03 +0100

    Forkortet enda mer

--- luke16.py
+++ luke16.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python
 
-candidates = []
 for var in range(100000, 500000):
     if sum((int(x) for x in str(var))) == 43:
-        candidates.append(var)
-[print(cnd) for cnd in candidates if int(round(cnd**(1/2)))**2 == cnd]
+        print(var) if int(round(var**(1/2)))**2 == var or int(round(var**(1/3)))**3 == var else None
