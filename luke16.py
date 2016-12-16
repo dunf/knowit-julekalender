273bfbaa1e9a65026919681ca95410de0bb93b5b
md <mihkal@dunfjeld.no>
2016-12-16 15:46:11 +0100

    Forkortet enda mer

--- luke16.py
+++ luke16.py
@@ -2,10 +2,6 @@
 
 candidates = []
 for var in range(100000, 500000):
-    sum = 0
-    for n in str(var):
-        sum += int(n)
-    if sum == 43:
+    if sum((int(x) for x in str(var))) == 43:
         candidates.append(var)
 [print(cnd) for cnd in candidates if int(round(cnd**(1/2)))**2 == cnd]
-
